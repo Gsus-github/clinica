4cff418c986ebe23ef91c690564769e931458bfa
Gsus <jesredondo@outlook.es>
2022-11-15 14:27:59 +0100

    Pruebas de conexión SQLite

--- Clinica.cs
+++ Clinica.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Xml;
+using Microsoft.Data.Sqlite;
+using System.IO;
 
 namespace ClinicaNS
 {
@@ -24,14 +26,10 @@ namespace ClinicaNS
             Console.WriteLine("DOCS: " + configuracion.rutaDocs);
             Console.WriteLine("BACKUP: " + configuracion.rutaBack);
 
-            /*
-            //Cargar el documento
-            doc.Load("config.xml");
-            //Carga los datos en la configuración.
-            XmlNode nodo = doc.DocumentElement.SelectSingleNode("//configuracion/rutas/ruta[@name='MITO']/direccion");
-            configuracion.rutaMito = nodo.InnerText;
-            Console.WriteLine("Ruta a MITO: " + configuracion.rutaMito);
-            */
+            //Crear la base de datos principal
+            BDDsqlite bdd = new BDDsqlite(configuracion.rutaBdd);
+            
+   
         }
 
         public static  Configuracion cargarConfiguracion(XmlDocument doc){
@@ -72,5 +70,55 @@ namespace ClinicaNS
         public String rutaBack{get; set;}="Vacio";
     }
 
+    public class BDDsqlite{
+
+        public BDDsqlite(String ruta){
+            ruta = Path.Combine(ruta, "clinica.db");
+            Console.WriteLine("Abriendo base de datos principal en: " + ruta);
+            if(!File.Exists(ruta)){
+                Console.WriteLine("No existe el fichero de BDD");
+            }else{
+                Console.WriteLine("El fichero de BDD EXISTE");
+                SqliteConnection sqlite_conexion = CreateConnection(ruta);
+                String consulta = "SELECT nombreUsuario FROM 'tblUsuarios'";
+                LeerDatos(sqlite_conexion, consulta);
+            }
+  
+        }
+        static SqliteConnection CreateConnection(String ruta)     {
+
+                SqliteConnection sqlite_conn;
+              
+                // Crear nueva conexión a la BDD
+                String cadenaConexion = "Data Source=" + ruta;
+                sqlite_conn = new SqliteConnection(cadenaConexion);
+                // Open the connection:
+                try
+                {
+                    sqlite_conn.Open();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.GetBaseException().ToString());
+                }
+                return sqlite_conn;
+        }
+
+        static void LeerDatos(SqliteConnection sqlite_conn, String consulta){
+            sqlite_conn.Open();
+            SqliteCommand comando = new SqliteCommand(consulta, sqlite_conn);
+            comando = sqlite_conn.CreateCommand();
+            //comando.CommandText=@"SELECT nombreUsuario FROM tblUsuarios";
+            comando.CommandText=("SELECT [nombreUsuario] FROM [main].[tblUsuarios]");
+            using (var lector = comando.ExecuteReader()){
+                while(lector.Read()){
+                    var nombre = lector.GetString(0);
+                    Console.WriteLine(nombre);
+                }
+            };
+            sqlite_conn.Close();
+        }
+
+    }
 
 }
